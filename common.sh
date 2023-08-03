9b951685e19cdbb68966e7b52345cc37aef443ac
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-08-03 18:17:44 +0530

    functions in robhoshop of shipping ans concising the code

--- common.sh
+++ common.sh
@@ -1,38 +1,70 @@
-nodejs()
+func_apprereq()
+{
+   echo -e "\e[34m>>>>>>>>>>>>>>> create application user<<<<<<<<<<<<<<\e[0m"
+
+   useradd roboshop  &${log}
+
+   echo -e "\e[31m>>>>>>>>>>>>>>> remove the old content<<<<<<<<<<<<<<\e[0m"
+   rm -rf /app  &${log}
+
+   echo -e "\e[35m>>>>>>>>>>>>>>> create application Directory<<<<<<<<<<<<<<\e[0m"
+
+   mkdir /app  &${log}
+   echo -e "\e[36m>>>>>>>>>>>>>>> download apllication content <<<<<<<<<<<<<<\e[0m"
+
+   curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &${log}
+    echo -e "\e[36m>>>>>>>>>>>>>>> extract apllication content <<<<<<<<<<<<<<\e[0m"
+   cd /app ${log}
+   unzip /tmp/$component.zip &${log}
+   cd /app &${log}
+}
+func_nodejs()
 {
  #variable declaration/
  log=/tmp/roboshop.log
 
  echo -e "\e[31m>>>>>>>>>>>>>>> create $component service <<<<<<<<<<<<<<\e[0m"
  cp  $component.service /etc/systemd/system/$component.service &${log}
- echo -e "\e[32m>>>>>>>>>>>>>>> create Mongo DB Repo<<<<<<<<<<<<<<\e[0m" | tee -a log
+ echo -e "\e[32m>>>>>>>>>>>>>>> create Mongo DB Repo<<<<<<<<<<<<<<\e[0m"
  cp  mongo.repo /etc/yum.repos.d/mongo.repo &${log}
- echo -e "\e[33m>>>>>>>>>>>>>>> Install Nodejs Repos<<<<<<<<<<<<<<\e[0m" | tee -a log
+ echo -e "\e[33m>>>>>>>>>>>>>>> Install Nodejs Repos<<<<<<<<<<<<<<\e[0m"
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &${log}
  echo -e ">>>>>>>>>>>>>>>  Install Nodejs <<<<<<<<<<<<<<\e[0m" | tee -a log
  yum install nodejs -y  &${log}
- echo -e "\e[34m>>>>>>>>>>>>>>> create application user<<<<<<<<<<<<<<\e[0m" | tee -a log
 
- useradd roboshop  &${log}
+ func_apprereq
 
- echo -e "\e[31m>>>>>>>>>>>>>>> remove the old content<<<<<<<<<<<<<<\e[0m" | tee -a log
- rm -rf /app  &${log}
+   echo -e "\e[36m>>>>>>>>>>>>>>> install dependencies <<<<<<<<<<<<<<\e[0m"
 
- echo -e "\e[35m>>>>>>>>>>>>>>> create application Directory<<<<<<<<<<<<<<\e[0m" | tee -a log
-
- mkdir /app  &${log}
- echo -e "\e[36m>>>>>>>>>>>>>>> download apllication content <<<<<<<<<<<<<<\e[0m" | tee -a log
-
- curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &${log}
- cd /app ${log}
- unzip /tmp/$component.zip &${log}
- cd /app &${log}
  npm install &${log}
  yum install mongodb-org-shell -y &${log}
  mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js &${log}
- systemctl daemon-reload &${log}
- systemctl enable $component &${log}
- systemctl restart $component &${log}
+func_systemd
+}
 
+func_systemd()
+{
+    echo -e "\e[31m>>>>>>>>>>>>>>> start $component service <<<<<<<<<<<<<<\e[0m"
+  systemctl daemon-reload &${log}
+  systemctl enable $component &${log}
+  systemctl restart $component &${log}
+  }
 
+func_java()
+{
+  echo -e "\e[31m>>>>>>>>>>>>>>> create $component service <<<<<<<<<<<<<<\e[0m"
+  cp $component.service /etc/systemd/system/$component.service
+  echo -e "\e[31m>>>>>>>>>>>>>>> install maven <<<<<<<<<<<<<<\e[0m"
+  yum install maven -
+
+   func_apprereq
+
+  echo -e "\e[31m>>>>>>>>>>>>>>> Build $component service <<<<<<<<<<<<<<\e[0m"
+  mvn clean package
+  mv target/$component-1.0.jar shipping.jar
+    echo -e "\e[31m>>>>>>>>>>>>>>> Install Mysql service <<<<<<<<<<<<<<\e[0m"
+  yum install mysql -y
+      echo -e "\e[31m>>>>>>>>>>>>>>> load schema <<<<<<<<<<<<<<\e[0m"
+  mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/$component.sql
+func_systemd
 }
