665d37ff38f26d57feaa53289f25ecd703d8f8fa
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-08-03 14:35:22 +0530

    rohoshop add the redirector output to the command

--- catalogue.sh
+++ catalogue.sh
@@ -1,31 +1,31 @@
 echo -e "\e[31m>>>>>>>>>>>>>>> create catalogue service <<<<<<<<<<<<<<\e[0m"
-cp  catalogue.service /etc/systemd/system/catalogue.service
+cp  catalogue.service /etc/systemd/system/catalogue.service >/tmp/robhoshop.log
 echo -e "\e[32m>>>>>>>>>>>>>>> create Mongo DB Repo<<<<<<<<<<<<<<\e[0m"
-cp  mongo.repo /etc/yum.repos.d/mongo.repo
+cp  mongo.repo /etc/yum.repos.d/mongo.repo >/tmp/robhoshop.log
 echo -e "\e[33m>>>>>>>>>>>>>>> Install Nodejs Repos<<<<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/robhoshop.log
 echo -e ">>>>>>>>>>>>>>>  Install Nodejs <<<<<<<<<<<<<<\e[0m"
-yum install nodejs -y
+yum install nodejs -y >/tmp/robhoshop.log >/tmp/robhoshop.log
 echo -e "\e[34m>>>>>>>>>>>>>>> create application user<<<<<<<<<<<<<<\e[0m"
 
-useradd roboshop
+useradd roboshop >/tmp/robhoshop.log >/tmp/robhoshop.log
 
 echo -e "\e[31m>>>>>>>>>>>>>>> remove the old content<<<<<<<<<<<<<<\e[0m"
-rm -rf /app
+rm -rf /app >/tmp/robhoshop.log >/tmp/robhoshop.log
 
 echo -e "\e[35m>>>>>>>>>>>>>>> create application Directory<<<<<<<<<<<<<<\e[0m"
 
-mkdir /app
+mkdir /app >/tmp/robhoshop.log >/tmp/robhoshop.log
 echo -e "\e[36m>>>>>>>>>>>>>>> download apllication content <<<<<<<<<<<<<<\e[0m"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-yum install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip >/tmp/robhoshop.log
+cd /app >/tmp/robhoshop.log
+unzip /tmp/catalogue.zip >/tmp/robhoshop.log
+cd /app >/tmp/robhoshop.log
+npm install >/tmp/robhoshop.log
+yum install mongodb-org-shell -y >/tmp/robhoshop.log
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js >/tmp/robhoshop.log
+systemctl daemon-reload >/tmp/robhoshop.log
+systemctl enable catalogue >/tmp/robhoshop.log
+systemctl restart catalogue >/tmp/robhoshop.log
 
